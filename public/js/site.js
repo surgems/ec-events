272454ee0acaa2238114eab1562fd32b4248958b
Jacob <developers@surgems.co.uk>
2021-11-02 15:11:34 +0000

    latest

--- public/js/site.js
+++ public/js/site.js
@@ -169,14 +169,21 @@ window.addEventListener('scroll', scrollAnim);
   \*********************************/
 /***/ (() => {
 
-$(document).ready(function () {
-  // BURGER MENU TOGGLE
-  $(".burger").on("click", function () {
-    $(this).children().first().toggleClass("open");
-    $("#mob-menu").toggleClass("active");
-    $("body").toggleClass("no-scroll");
-  });
-});
+// BURGER MENU TOGGLE
+$(".burger").on("click", function () {
+  $(this).children().first().toggleClass("open");
+  $("#mob-menu").toggleClass("active");
+  $("body").toggleClass("no-scroll");
+}); // NAV DROPDOWN 
+// $(".nav-link").on("click", function() {
+//   const isOpened = $(this).find(".is-opened")[0]
+//   $(this).toggleClass("active");
+//   if ($(this).hasClass('active')) {
+//     isOpened.innerHTML = '-';
+//   } else {
+//     isOpened.innerHTML = '+';
+//   };
+// });
 
 /***/ }),
 
--- resources/js/globals.js
+++ resources/js/globals.js
@@ -1,10 +1,19 @@
-$(document).ready(function () {
+// BURGER MENU TOGGLE
 
-  // BURGER MENU TOGGLE
-  $(".burger").on("click", function() {
-    $(this).children().first().toggleClass("open");
-    $("#mob-menu").toggleClass("active");
-    $("body").toggleClass("no-scroll");
-  });
-
+$(".burger").on("click", function() {
+  $(this).children().first().toggleClass("open");
+  $("#mob-menu").toggleClass("active");
+  $("body").toggleClass("no-scroll");
 });
+
+// NAV DROPDOWN 
+
+// $(".nav-link").on("click", function() {
+//   const isOpened = $(this).find(".is-opened")[0]
+//   $(this).toggleClass("active");
+//   if ($(this).hasClass('active')) {
+//     isOpened.innerHTML = '-';
+//   } else {
+//     isOpened.innerHTML = '+';
+//   };
+// });
